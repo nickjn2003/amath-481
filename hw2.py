6a8d853a62670d16d0067fa16e6080b88acc9dd7
Nicholas <nickjn2003@yahoo.com>
2024-10-18 12:53:29 -0700

    second

--- hw2.py
+++ hw2.py
@@ -8,7 +8,7 @@ def shoot2(phi, dummy, n0, epsilon):
 
 tol = 1e-4  # define a tolerance level 
 col = ['r', 'b', 'g', 'c', 'm', 'k']  # eigenfunc colors
-n0 = 10; A = 1; x0 = [0, A]; xp = [-4, 4] 
+n0 = 16; A = 1; xp = [-4, 4] 
 xshoot =  np.linspace(xp[0], xp[1], 81)
 A1 = np.zeros((81, 5))
 A2 = []
@@ -18,9 +18,10 @@ for modes in range(1, 6):  # begin mode loop
     epsilon = epsilon_start  # initial value of eigenvalue beta
     depsilon = n0 / 100  # default step size in beta
     for _ in range(1000):  # begin convergence loop for beta
+        x0 = [1, np.sqrt(4**2 - epsilon)]
         y = odeint(shoot2, x0, xshoot, args=(n0, epsilon)) 
-       # y = RK45(shoot2, xp[0], x0, xp[1], args=(n0,beta)) 
-        print(y)
+        # y = RK45(shoot2, xp[0], x0, xp[1], args=(n0,beta)) 
+
         if abs(y[-1, 1] + np.sqrt(4**2 - epsilon) * y[-1, 0]) < tol:  # check for convergence
             A2.append(epsilon)
             # print(epsilon)  # write out eigenvalue
@@ -32,7 +33,7 @@ for modes in range(1, 6):  # begin mode loop
             epsilon -= depsilon / 2
             depsilon /= 2
 
-    epsilon_start = epsilon - 0.1  # after finding eigenvalue, pick new start
+    epsilon_start = epsilon + 0.1  # after finding eigenvalue, pick new start
     norm = np.trapz(np.abs(y[:, 0]) * np.abs(y[:, 0]), xshoot)
     A1[:, modes - 1] = np.abs(y[:, 0] / np.sqrt(norm))
     plt.plot(xshoot, y[:, 0] / np.sqrt(norm), col[modes - 1], label=f'ϕ{modes}')  # plot modes
